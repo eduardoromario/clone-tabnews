0d43a682bb70703b0c58ec81fbfa34e6aa558695
Eduardo Romário <duu.007@gmail.com>
2025-12-01 16:57:56 -0300

    chore(database): makes database.js more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -8,7 +8,15 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
   });
-  await client.connect();
+
+  try {
+    await client.connect();
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await client.end();
+  }
 
   try {
     const result = await client.query(queryObject);
