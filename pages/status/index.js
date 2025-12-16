76223503235caacbb342fe64465df2fb8f847420
Eduardo Romário <duu.007@gmail.com>
2025-12-16 16:37:38 -0300

    feat: add initial version of `status` page

---
+++ pages/status/index.js
@@ -0,0 +1,55 @@
+import useSWR from "swr";
+
+async function fetchAPI(key) {
+  const response = await fetch(key);
+  const responseBody = await response.json();
+  return responseBody;
+}
+
+export default function StatusPage() {
+  return (
+    <>
+      <h1>Status</h1>
+      <UpdatedAt />
+      <DatabaseStatus />
+    </>
+  );
+
+  function UpdatedAt() {
+    const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+      refreshInterval: 2000,
+    });
+
+    let updatedAtText = "Carregando...";
+
+    if (!isLoading && data) {
+      updatedAtText = new Date(data.updated_at).toLocaleString("pt-BR");
+    }
+
+    return <p>♻️ Last update: {updatedAtText}</p>;
+  }
+
+  function DatabaseStatus() {
+    const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+      refreshInterval: 2000,
+    });
+
+    let databaseStatus = "Loading...";
+    if (!isLoading && data) {
+      databaseStatus = (
+        <>
+          <h2>Database</h2>
+          <p>🗃️ Version: {data.dependencies.database.version}</p>
+          <p>
+            📈 Opened Connections:{" "}
+            {data.dependencies.database.current_connections}
+          </p>
+          <p>
+            🔒 Max Connections: {data.dependencies.database.max_connections}
+          </p>
+        </>
+      );
+    }
+    return databaseStatus;
+  }
+}
